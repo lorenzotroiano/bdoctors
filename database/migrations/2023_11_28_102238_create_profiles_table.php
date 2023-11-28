636d603653eef5bf8dc74eca5c8aaa70e9a127ab
lorenzotroiano <lorenzo.troiano1999@gmail.com>
2023-11-28 11:42:07 +0100

    first commit

---
+++ database/migrations/2023_11_28_102238_create_profiles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+
+            $table->text('description');
+            $table->string('services');
+            $table->text('address');
+            $table->string('photo');
+            $table->boolean('visible');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
