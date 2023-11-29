0e17b89f203451824ccd856da9feacadaf43b2a9
lorenzotroiano <lorenzo.troiano1999@gmail.com>
2023-11-29 14:33:17 +0100

    riempito il db con i seeders

--- database/seeders/StarTableSeeder.php
+++ database/seeders/StarTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Star;
+use App\Models\Profile;
 
 class StarTableSeeder extends Seeder
 {
@@ -14,6 +16,32 @@ class StarTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $starData = [
+            ['vote' => '1'],
+            ['vote' => '2'],
+            ['vote' => '3'],
+            ['vote' => '4'],
+            ['vote' => '5'],
+            ['vote' => '6'],
+            ['vote' => '7'],
+            ['vote' => '8'],
+            ['vote' => '9'],
+            ['vote' => '10'],
+
+        ];
+
+        foreach ($starData as $data) {
+            Star::create($data);
+        };
+
+        $profiles = Profile::inRandomOrder()->limit(rand(1, 3))->get();
+
+        $starVotes = array_column($starData, 'vote');
+
+        $stars = Star::whereIn('vote', $starVotes)->get();
+
+        foreach ($stars as $star) {
+            $star->profiles()->attach($profiles);
+        }
     }
 }
