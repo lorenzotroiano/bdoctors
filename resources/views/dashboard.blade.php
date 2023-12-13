58d02b77745634fdd0234d588bb268f243fc8196
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-12-13 10:41:59 +0100

    add delete button

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+    <form action="{{ route('profile.doctor.destroy') }}" method="POST">
+        @csrf
+        @method('DELETE')
+        <button class="delete-btn btn btn-danger m-1" type="submit"><i class="fa-solid fa-trash-can"></i></button>
+    </form>
 @endsection
